f4fc1e26fd3a4e348769f208d6167cf1ade8fac2
Teshome Mosneh <teshome.mosneh@gmail.com>
2025-05-10 23:47:11 +0300

    Intial Commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+  <div className=' bg-blue-500 text-red-500'>Hello World</div>
+  )
+}
+
+export default App
